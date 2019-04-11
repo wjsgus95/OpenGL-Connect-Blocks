cd3d5b2746e90cdc522d4689cb30ff429d22ff11
wjsgus95 <wjsgus95@gmail.com>
2019-04-11 21:51:38 +0900

    grid constants and epsilon added

--- utils/def.h
+++ utils/def.h
@@ -1,12 +1,14 @@
 #ifndef __DEF_H__
 #define __DEF_H__
 
+#define EPSILON 1e-6f
+
 #define TABLE_HEIGHT 0.15f
 
 #define CUBE_EDGE 0.5f
 #define CUBE_HALF_EDGE 0.25f
 
-#define GRID 0.5f
+#define ONE_GRID 0.5f
 
 #define GRID_START_X 2.25f
 #define GRID_START_Y -2.25f
